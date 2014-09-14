6e81c300d64cf6d73749dbdac28f4307338be02c
Nicolas Arkkila <nicolas@arkkila.fi>
2014-09-14 20:31:09 +0300

    Spring things

--- viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -5,15 +5,22 @@ import ohtu.verkkokauppa.Kirjanpito;
 import ohtu.verkkokauppa.Pankki;
 import ohtu.verkkokauppa.Varasto;
 import ohtu.verkkokauppa.Viitegeneraattori;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
+        
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+
+        
         Kirjanpito kirjanpito      = new Kirjanpito();
         Varasto varasto            = new Varasto(kirjanpito);
         Pankki pankki              = new Pankki(kirjanpito);
         Viitegeneraattori viitegen = new Viitegeneraattori();
-        Kauppa kauppa              = new Kauppa(varasto, pankki, viitegen);
+        Kauppa kauppa = ctx.getBean(Kauppa.class);
+        
         
 
 
