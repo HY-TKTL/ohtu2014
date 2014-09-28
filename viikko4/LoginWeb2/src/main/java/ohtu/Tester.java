a68effb7f417a601878eac3b29df8c9508be03b6
makroma <markoma@iki.fi>
2014-09-28 14:26:05 +0300

    viikko4 commit

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -6,26 +6,77 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
+
     public static void main(String[] args) {
         WebDriver driver = new HtmlUnitDriver();
 
+        rightCredententials(driver);
+        wrongPass(driver);
+        newUser(driver);
+
+    }
+
+    private static void rightCredententials(WebDriver driver) {
         driver.get("http://localhost:8090");
-        System.out.println( driver.getPageSource() );
-        WebElement element = driver.findElement(By.linkText("login"));       
-        element.click(); 
-        
+        System.out.println(driver.getPageSource());
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
         System.out.println("==");
-        
-        System.out.println( driver.getPageSource() );
+
+        System.out.println(driver.getPageSource());
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
         element.submit();
-        
+
         System.out.println("==");
-        System.out.println( driver.getPageSource() );
+        System.out.println(driver.getPageSource());
+    }
+
+    private static void wrongPass(WebDriver driver) {
+        driver.get("http://localhost:8090");
+        System.out.println(driver.getPageSource());
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        System.out.println("==");
+
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("1234");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
+    }
+
+    private static void newUser(WebDriver driver) {
+        driver.get("http://localhost:8090");
+        System.out.println(driver.getPageSource());
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        System.out.println("==");
+
+        System.out.println(driver.getPageSource());
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("jorma");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("12345678");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("12345678");
         
+        
+        element = driver.findElement(By.name("add"));
+        element.submit();
+
+        System.out.println("==");
+        System.out.println(driver.getPageSource());
     }
 }
